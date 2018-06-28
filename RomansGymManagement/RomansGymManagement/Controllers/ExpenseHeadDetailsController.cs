79ec8d45e6e168fc17a7ee8c729538147a3bfc40
Aathira <aathirakb90@gmail.com>
2018-06-28 23:15:18 +0530

    Initial project
    Signed-off-by: Aathira <aathirakb90@gmail.com>

---
+++ RomansGymManagement/RomansGymManagement/Controllers/ExpenseHeadDetailsController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RomansGymManagement.Models;
+
+namespace RomansGymManagement.Controllers
+{
+    [Route("api/Expense")]
+    public class ExpenseHeadDetailsController : ApiController
+    {
+        private romansgy_gymEntities db = new romansgy_gymEntities();
+
+        // GET: api/ExpenseHeadDetails
+        public IQueryable<ExpenseHeadDetail> GetExpenseHeadDetails()
+        {
+            return db.ExpenseHeadDetails;
+        }
+
+        // GET: api/ExpenseHeadDetails/5
+        [ResponseType(typeof(ExpenseHeadDetail))]
+        public IHttpActionResult GetExpenseHeadDetail(int id)
+        {
+            ExpenseHeadDetail expenseHeadDetail = db.ExpenseHeadDetails.Find(id);
+            if (expenseHeadDetail == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(expenseHeadDetail);
+        }
+
+        // PUT: api/ExpenseHeadDetails/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutExpenseHeadDetail(int id, ExpenseHeadDetail expenseHeadDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != expenseHeadDetail.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(expenseHeadDetail).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExpenseHeadDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/ExpenseHeadDetails
+        [ResponseType(typeof(ExpenseHeadDetail))]
+        [Route("api/Expense", Name = "InsertExpenseDetails")]
+        public IHttpActionResult PostExpenseHeadDetail(ExpenseHeadDetail expenseHeadDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.ExpenseHeadDetails.Add(expenseHeadDetail);
+            db.SaveChanges();
+
+            return CreatedAtRoute("InsertExpenseDetails", new { id = expenseHeadDetail.ID }, expenseHeadDetail);
+        }
+        [Route("api/Expense/getLiju")]
+        [ResponseType(typeof(ExpenseHeadDetail))]
+        public string GetExpenseHeadDetail()
+        {
+            string liju = "liju";
+            return liju;
+        }
+
+
+        // DELETE: api/ExpenseHeadDetails/5
+        [ResponseType(typeof(ExpenseHeadDetail))]
+        [Route("api/Expense/{id:int}")]
+        public IHttpActionResult DeleteExpenseHeadDetail(int id)
+        {
+            ExpenseHeadDetail expenseHeadDetail = db.ExpenseHeadDetails.Find(id);
+            if (expenseHeadDetail == null)
+            {
+                return NotFound();
+            }
+
+            db.ExpenseHeadDetails.Remove(expenseHeadDetail);
+            db.SaveChanges();
+
+            return Ok(expenseHeadDetail);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ExpenseHeadDetailExists(int id)
+        {
+            return db.ExpenseHeadDetails.Count(e => e.ID == id) > 0;
+        }
+    }
+}
---
+++ RomansGymManagement/RomansGymManagement/Controllers/IncomeDetailsController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RomansGymManagement.Models;
+
+namespace RomansGymManagement.Controllers
+{
+ [Route("api/Income")]
+    public class IncomeDetailsController : ApiController
+    {
+        private romansgy_gymEntities db = new romansgy_gymEntities();
+
+        // GET: api/IncomeDetails
+        public IQueryable<IncomeDetail> GetIncomeDetails()
+        {
+            return db.IncomeDetails;
+        }
+
+        // GET: api/IncomeDetails/5
+        [ResponseType(typeof(IncomeDetail))]
+        public IHttpActionResult GetIncomeDetail(int id)
+        {
+            IncomeDetail incomeDetail = db.IncomeDetails.Find(id);
+            if (incomeDetail == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(incomeDetail);
+        }
+
+        // PUT: api/IncomeDetails/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutIncomeDetail(int id, IncomeDetail incomeDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != incomeDetail.ID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(incomeDetail).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!IncomeDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/IncomeDetails
+        [ResponseType(typeof(IncomeDetail))]
+        [Route("api/Income", Name = "InsertIncomeDetails")]
+        public IHttpActionResult PostIncomeDetail(IncomeDetail incomeDetail)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.IncomeDetails.Add(incomeDetail);
+            db.SaveChanges();
+
+            return CreatedAtRoute("InsertExpenseDetails", new { id = incomeDetail.ID }, incomeDetail);
+        }
+
+        // DELETE: api/IncomeDetails/5
+        [ResponseType(typeof(IncomeDetail))]
+        public IHttpActionResult DeleteIncomeDetail(int id)
+        {
+            IncomeDetail incomeDetail = db.IncomeDetails.Find(id);
+            if (incomeDetail == null)
+            {
+                return NotFound();
+            }
+
+            db.IncomeDetails.Remove(incomeDetail);
+            db.SaveChanges();
+
+            return Ok(incomeDetail);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool IncomeDetailExists(int id)
+        {
+            return db.IncomeDetails.Count(e => e.ID == id) > 0;
+        }
+    }
+}
